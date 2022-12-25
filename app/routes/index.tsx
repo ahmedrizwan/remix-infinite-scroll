418d62a5685891e10e6420cb7bb78a92663a7028
Ahmed Rizwan <ahmed.rizwan@epicgames.quixel.com>
2022-12-25 16:13:00 +0500

    feat: extract infinite scroll into a component

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,6 @@
 import { LoaderFunction } from "@remix-run/node";
 import { useFetcher, useLoaderData } from "@remix-run/react";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Data, fetchItems, ItemsResponse } from "~/api.server";
 
 export const loader: LoaderFunction = async (remixContext) => {
@@ -14,23 +14,46 @@ export const loader: LoaderFunction = async (remixContext) => {
   return items;
 };
 
-export default function Index() {
-  const initialItems = useLoaderData<ItemsResponse>();
-  const fetcher = useFetcher<ItemsResponse>();
+const InfiniteScroller = (props: {
+  children: any;
+  loading: boolean;
+  loadNext: () => void;
+}) => {
+  const { children, loading, loadNext } = props;
+  const scrollListener = useRef(loadNext);
 
-  const [items, setItems] = useState<Data[]>(initialItems.data);
+  useEffect(() => {
+    scrollListener.current = loadNext;
+  }, [loadNext]);
 
   const onScroll = () => {
     const documentHeight = document.documentElement.scrollHeight;
     const scrollDifference = Math.floor(window.innerHeight + window.scrollY);
     const scrollEnded = documentHeight == scrollDifference;
 
-    if (scrollEnded && fetcher.state === "idle") {
-      const page = fetcher.data ? fetcher.data.page + 1 : initialItems.page + 1;
-      const query = `?index&page=${page}`;
-      fetcher.load(query);
+    if (scrollEnded && !loading) {
+      scrollListener.current();
     }
   };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", onScroll);
+    }
+
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, []);
+
+  return <>{children}</>;
+};
+
+export default function Index() {
+  const initialItems = useLoaderData<ItemsResponse>();
+  const fetcher = useFetcher<ItemsResponse>();
+
+  const [items, setItems] = useState<Data[]>(initialItems.data);
 
   useEffect(() => {
     if (!fetcher.data || fetcher.state === "loading") {
@@ -43,40 +66,43 @@ export default function Index() {
     }
   }, [fetcher.data]);
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      window.addEventListener("scroll", onScroll);
-    }
-
-    return () => {
-      window.removeEventListener("scroll", onScroll);
-    };
-  }, [fetcher.state]);
-
   return (
-    <div
-      style={{
-        fontFamily: "system-ui, sans-serif",
-        lineHeight: "1.4",
+    <InfiniteScroller
+      loadNext={() => {
+        const page = fetcher.data
+          ? fetcher.data.page + 1
+          : initialItems.page + 1;
+        const query = `?index&page=${page}`;
+        console.log(query);
+        fetcher.load(query);
       }}
+      loading={fetcher.state === "loading"}
     >
-      {/* Items Grid */}
-      <div className="items-container">
-        {items.map((item) => (
-          <img key={item.id} className="item" src={item.thumb} />
-        ))}
+      <div
+        style={{
+          fontFamily: "system-ui, sans-serif",
+          lineHeight: "1.4",
+        }}
+      >
+        {/* Items Grid */}
+        <div className="items-container">
+          {items.map((item) => (
+            <img key={item.id} className="item" src={item.thumb} />
+          ))}
+        </div>
+
+        {/* Loader */}
+        <div className="loader-container">
+          {fetcher.state === "loading" && (
+            <div className="lds-ellipsis">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          )}
+        </div>
       </div>
-      {/* Loader */}
-      <div className="loader-container">
-        {fetcher.state === "loading" && (
-          <div className="lds-ellipsis">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
-        )}
-      </div>
-    </div>
+    </InfiniteScroller>
   );
 }
