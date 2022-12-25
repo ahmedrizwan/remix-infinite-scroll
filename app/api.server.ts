bfa08c4b2ed5f07e29d9ab0b9098da04385f752b
Ahmed Rizwan <ahmed.rizwan@epicgames.quixel.com>
2022-12-25 05:31:35 +0500

    infinite scroll with remix

---
+++ app/api.server.ts
@@ -0,0 +1,20 @@
+export type Data = { id: number; thumb: string };
+export type ItemsResponse = { data: Data[]; page: number };
+
+export const fetchItems = async (query: {
+  page: number;
+}): Promise<ItemsResponse> => {
+  const start = query.page * 30;
+
+  const items = Array.from({ length: 30 }, (_, i) => i + start).map((id) => ({
+    id,
+    thumb: `https://picsum.photos/200?${id}`,
+  }));
+
+  await new Promise((r) => setTimeout(r, 2000));
+
+  return Promise.resolve({
+    data: items,
+    page: query.page,
+  });
+};
---
+++ app/routes/index.tsx
@@ -0,0 +1,82 @@
+import { LoaderFunction } from "@remix-run/node";
+import { useFetcher, useLoaderData } from "@remix-run/react";
+import { useEffect, useState } from "react";
+import { Data, fetchItems, ItemsResponse } from "~/api.server";
+
+export const loader: LoaderFunction = async (remixContext) => {
+  const url = new URL(remixContext.request.url);
+  const page = url.searchParams.get("page") || 0;
+
+  const items = await fetchItems({
+    page: Number(page),
+  });
+
+  return items;
+};
+
+export default function Index() {
+  const initialItems = useLoaderData<ItemsResponse>();
+  const fetcher = useFetcher<ItemsResponse>();
+
+  const [items, setItems] = useState<Data[]>(initialItems.data);
+
+  const onScroll = () => {
+    const documentHeight = document.documentElement.scrollHeight;
+    const scrollDifference = Math.floor(window.innerHeight + window.scrollY);
+    const scrollEnded = documentHeight == scrollDifference;
+
+    if (scrollEnded && fetcher.state === "idle") {
+      const page = fetcher.data ? fetcher.data.page + 1 : initialItems.page + 1;
+      const query = `?index&page=${page}`;
+      fetcher.load(query);
+    }
+  };
+
+  useEffect(() => {
+    if (!fetcher.data || fetcher.state === "loading") {
+      return;
+    }
+
+    if (fetcher.data) {
+      const newItems = fetcher.data.data;
+      setItems((prevAssets) => [...prevAssets, ...newItems]);
+    }
+  }, [fetcher.data]);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", onScroll);
+    }
+
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, [fetcher.state]);
+
+  return (
+    <div
+      style={{
+        fontFamily: "system-ui, sans-serif",
+        lineHeight: "1.4",
+      }}
+    >
+      {/* Items Grid */}
+      <div className="items-container">
+        {items.map((item) => (
+          <img key={item.id} className="item" src={item.thumb} />
+        ))}
+      </div>
+      {/* Loader */}
+      <div className="loader-container">
+        {fetcher.state === "loading" && (
+          <div className="lds-ellipsis">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
